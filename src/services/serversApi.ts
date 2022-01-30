d0c1180e1ce9971d8cea70b217ca0a043dcae28a
Joe <efessel@gmail.com>
2022-01-29 20:27:42 -0500

    un-stub it oops

--- src/services/serversApi.ts
+++ src/services/serversApi.ts
@@ -2,15 +2,13 @@ import { Match } from "@/model/Match"
 import { PagedResult } from "@/model/PagedResult"
 import { ServerStatus } from "@/model/ServerStatus"
 import axios from "axios"
-import status from '../../test/status.json'
 
 const apiHost = process.env.VUE_APP_SERVERS_API
 
 export const getStatus = (): Promise<ServerStatus[]> => {
-  return Promise.resolve(status as any);
-  // return axios
-  //   .get(`${apiHost}/status`)
-  //   .then(response => response.data)
+  return axios
+    .get(`${apiHost}/status`)
+    .then(response => response.data)
 }
 
 export const getRecentMatches = (): Promise<Match[]> => {
